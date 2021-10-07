d34c81835570cc05a07623257c8b0950e6ad16a3
Lance Aranas <laranas_200000000257@uic.edu.ph>
2021-10-07 16:53:57 +0800

    second commit with database queries

---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "lastname1lastname2"
+});
+
+module.exports = pool;
